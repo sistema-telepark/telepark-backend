7e254b6fa3ebc4f7114f0089f56486df6b2b660c
Elias Brizuela <elias.brizuela@absanet.com>
2022-05-06 22:31:53 -0300

    static root

--- telepark/settings.py
+++ telepark/settings.py
@@ -11,14 +11,25 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import mimetypes
 from pathlib import Path
 from datetime import timedelta
 from dotenv import load_dotenv
 
 load_dotenv()
+mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 # Quick-start development settings - unsuitable for production
