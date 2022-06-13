e90a540d32b7ebc4e262e8d838482f599ab10b69
Jonathan Sergio Rodriguez Alvarez <joni_la12_elnacio@hotmail.com>
2022-06-13 05:59:39 -0300

    Cree un endpoint para actualizar la cuenta de un usuario.

--- teleparkApi/authentication.py
+++ teleparkApi/authentication.py
@@ -1,6 +1,8 @@
+from urllib import response
 from django.contrib.auth.models import User
+from django.http import JsonResponse
 from rest_framework.decorators import api_view, permission_classes
-from django.contrib.auth.hashers import check_password
+from django.contrib.auth.hashers import check_password, make_password
 from .permission import IsSuperuser
 from .helpers import check_attributes
 from .static import HTTP_METHOD
@@ -54,3 +56,39 @@ def create_user(request):
                              is_superuser=body['is_superuser'], is_staff=body['is_staff'], email=body['email'])
 
     return Response('OK', status=status.HTTP_201_CREATED)
+
+@api_view([HTTP_METHOD.PUT])
+@permission_classes([IsSuperuser])
+def update_user(request):
+    if(not request.user.is_superuser):
+        return Response('No posee los permisos requeridos', status.HTTP_401_UNAUTHORIZED)
+
+    body = json.loads(request.body.decode('utf-8'))
+    if (request.method != HTTP_METHOD.PUT):
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    if (not check_attributes(body, ['user', 'first_name', 'last_name', 'is_superuser', 'is_active'])):
+        return Response('Datos incompletos', status=status.HTTP_400_BAD_REQUEST)
+
+    if (check_attributes(body, ['password'])):
+        User.objects.filter(username=body['user']).update(first_name=body['first_name'], last_name=body['last_name'],password=make_password(body['password']), is_superuser=body['is_superuser'], is_active=body['is_active'])
+    
+    if (not check_attributes(body, ['password'])):
+        User.objects.filter(username=body['user']).update(first_name=body['first_name'], last_name=body['last_name'], is_superuser=body['is_superuser'], is_active=body['is_active'])
+        
+    response = User.objects.filter(username=body['user']).values()
+
+    return Response({response}, status=status.HTTP_201_CREATED)
+
+
+@api_view([HTTP_METHOD.GET])
+@permission_classes([IsSuperuser])
+def get_users(request):
+    if(not request.user.is_superuser):
+        return Response('No posee los permisos requeridos', status.HTTP_401_UNAUTHORIZED)
+
+    if (request.method != HTTP_METHOD.GET):
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    response = ([{'username': user.username, 'first_name':user.first_name, 'last_name':user.last_name, 'name': user.get_full_name(), 'is_superuser': user.is_superuser, 'is_active': user.is_active} for user in User.objects.all()])
+    return Response(response, status=status.HTTP_200_OK)
--- teleparkApi/urls.py
+++ teleparkApi/urls.py
@@ -33,6 +33,8 @@ urlpatterns = [
     re_path(r'^api/login$', authentication.auth_view),
     re_path(r'^api/create_user$', authentication.create_user),
     re_path('api/refresh_token', TokenRefreshView.as_view(), name='token_refresh'),
+    re_path(r'^api/users$', authentication.get_users),
+    re_path(r'^api/update_user$', authentication.update_user),
 ]
 
 urlpatterns += router.urls
